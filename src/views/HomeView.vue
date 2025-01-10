172479fa3827d6acc65dda01494c5b25d40a42b3
facuagueria <fagueria@sugit.com.ar>
2025-01-10 16:00:08 -0300

    chore: first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen bg-red-300">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
