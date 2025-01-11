30613edd28a815f88084a092a390fcffa2d2308f
facuagueria <fagueria@sugit.com.ar>
2025-01-11 19:04:32 -0300

    feat(components): install shadcn-vue components

---
+++ src/components/ui/rating/AppRater.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import StarRating from 'vue-star-rating'
+
+const modelValue = defineModel<{
+  rating: number
+}>()
+</script>
+
+<template>
+  <StarRating
+    v-model="modelValue"
+    :rounded-corners="true"
+    :star-size="20"
+    :max-rating="10"
+    :show-rating="true"
+  />
+</template>
+
+<style></style>
