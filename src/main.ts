edbe5d1deb1c1a4f65dd33759c55290001bda677
facuagueria <fagueria@sugit.com.ar>
2025-01-12 17:12:44 -0300

    feat(charts): install chart.js and vue-chartjs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 const app = createApp(App)
 
 app.use(createPinia())
