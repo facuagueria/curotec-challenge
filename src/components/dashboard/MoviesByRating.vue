0902960151d798f2353f7b37edcf1401a335d543
facuagueria <fagueria@sugit.com.ar>
2025-01-12 17:20:52 -0300

    style(dashboard): add colors to movies-by-rating chart

--- src/components/dashboard/MoviesByRating.vue
+++ src/components/dashboard/MoviesByRating.vue
@@ -21,6 +21,18 @@ const chartData = computed(() => ({
     {
       label: 'Movies by Rating',
       data: props.moviesByRating.map((movie) => movie.count),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+      ],
     },
   ],
 }))
